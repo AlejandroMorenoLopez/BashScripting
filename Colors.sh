130fa792ff068027979b1954ec3ca9d1d661962e
Alejandro Moreno <126996993+AlejandroMorenoLopez@users.noreply.github.com>
2023-06-24 15:24:59 -0600

    Add files via upload

---
+++ Colors.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+#Colors and styles
+blackNormal="\e[0;30;30m"
+blackBold="\e[0;30m\e[1m"
+blackItalics="\e[0;30m\e[3m"
+blackUnderlined="\e[0;30m\e[4m"
+
+blueNormal="\e[0;34m"
+blueBold="\e[0;34m\e[1m"
+blueItalics="\e[0;34m\e[3m"
+blueUnderlined="\e[0;34m\e[4m"
+
+cyanNormal="\e[0;36m"
+cyanBold="\e[0;36m\e[1m"
+cyanItalics="\e[0;36m\e[3m"
+cyanUnderlined="\e[0;36m\e[4m"
+
+yellowNormal="\e[0;33m"
+yellowBold="\e[0;33m\e[1m"
+yellowItalics="\e[0;33m\e[3m"
+yellowUnderlined="\e[0;33m\e[4m"
+
+greenNormal="\e[0;32m"
+greenBold="\e[0;32m\e[1m"
+greenItalics="\e[0;32m\e[3m"
+greenUnderlined="\e[0;32m\e[4m"
+
+redNormal="\e[1;31m"
+redBold="\e[1;31m\e[1m"
+redItalics="\e[1;31m\e[3m"
+redUnderlined="\e[1;31m\e[4m"
+
+purpleNormal="\e[0;35m"
+purpleBold="\e[0;35m\e[1m"
+purpleItalics="\e[0;35m\e[3m"
+purpleUnderlined="\e[0;35m\e[4m"
+
+grayNormal="\e[0:37m"
+grayBold="\e[0;37m\e[1m"
+grayItalics="\e[0;37m\e[3m"
+grayUnderlined="\e[0;37m\e[4m"
+
+Reset="\e[0m"
+
+  echo -e "\n${blackNormal}[+] Esto es ayuda en Negro Normal${Reset}"
+  echo -e "${blackBold}[+] Esto es ayuda en Negro Negritas${Reset}"
+  echo -e "${blackItalics}[+] Esto es ayuda en Negro Italicas${Reset}"
+  echo -e "${blackUnderlined}[+] Esto es ayuda en Negro Subrayados${Reset}"
+
+  echo -e "\n${purpleNormal}[+] Esto es ayuda en Purpura Normal${Reset}"
+  echo -e "${purpleBold}[+] Esto es ayuda en Purpura Negritas${Reset}"
+  echo -e "${purpleItalics}[+] Esto es ayuda en Purpura Italicas${Reset}"
+  echo -e "${purpleUnderlined}[+] Esto es ayuda en Purpura Subrayados${Reset}"
+
+  echo -e "\n${blueNormal}[+] Esto es ayuda en Azul Normal${Reset}"
+  echo -e "${blueBold}[+] Esto es ayuda en Azul Negritas${Reset}"
+  echo -e "${blueItalics}[+] Esto es ayuda en Azul Italicas${Reset}"
+  echo -e "${blueUnderlined}[+] Esto es ayuda en Azul Subrayados${Reset}"
+
+  echo -e "\n${cyanNormal}[+] Esto es ayuda en Cyan Normal${Reset}"
+  echo -e "${cyanBold}[+] Esto es ayuda en Cyan Negritas${Reset}"
+  echo -e "${cyanItalics}[+] Esto es ayuda en Cyan Italicas${Reset}"
+  echo -e "${cyanUnderlined}[+] Esto es ayuda en Cyan Subrayados${Reset}"
+
+  echo -e "\n${greenNormal}[+] Esto es ayuda en Verde Normal${Reset}"
+  echo -e "${greenBold}[+] Esto es ayuda en Verde Negritas${Reset}"
+  echo -e "${greenItalics}[+] Esto es ayuda en Verde Italicas${Reset}"
+  echo -e "${greenUnderlined}[+] Esto es ayuda en Verde Subrayados${Reset}"
+
+  echo -e "\n${yellowNormal}[+] Esto es ayuda en Amarillo Normal${Reset}"
+  echo -e "${yellowBold}[+] Esto es ayuda en Amarillo Negritas${Reset}"
+  echo -e "${yellowItalics}[+] Esto es ayuda en Amarillo Italicas${Reset}"
+  echo -e "${yellowUnderlined}[+] Esto es ayuda en Amarillo Subrayados${Reset}"
+
+  echo -e "\n${redNormal}[+] Esto es ayuda en Rojo Normal${Reset}"
+  echo -e "${redBold}[+] Esto es ayuda en Rojo Negritas${Reset}"
+  echo -e "${redItalics}[+] Esto es ayuda en Rojo Italicas${Reset}"
+  echo -e "${redUnderlined}[+] Esto es ayuda en Rojo Subrayados${Reset}"
+
+  echo -e "\n${grayNormal}[+] Esto es ayuda en Gris Normal${Reset}"
+  echo -e "${grayBold}[+] Esto es ayuda en Gris Negritas${Reset}"
+  echo -e "${grayItalics}[+] Esto es ayuda en Gris Italicas${Reset}"
+  echo -e "${grayUnderlined}[+] Esto es ayuda en Gris Subrayados${Reset}"
